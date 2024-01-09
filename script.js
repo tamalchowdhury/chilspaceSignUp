65589db2ef2245eac614e4357a4bcbefd2f36b0c
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-01-09 16:24:07 +0600

    adds prettier

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-const toggleShowButton = document.querySelector(".form__toggle_password")
+const toggleShowButton = document.querySelector(".form__toggle_password");
 
-toggleShowButton.addEventListener("click", togglePassword)
+toggleShowButton.addEventListener("click", togglePassword);
 
 function togglePassword(event) {
+  const passwordField = document.querySelector("#new-password");
+  const currentType = passwordField.getAttribute("type");
+  const showText = document.getElementById("showText");
+  const eyeIcon = document.querySelector(".fas");
 
-  const passwordField = document.querySelector("#new-password")
-  const currentType = passwordField.getAttribute("type")
-  const showText = document.getElementById("showText")
-  const eyeIcon = document.querySelector(".fas")
-
-  if(currentType === "password") {
-    passwordField.setAttribute("type", "text")
-    showText.textContent = "HIDE"
-    eyeIcon.classList.remove("fa-eye-slash")
-    eyeIcon.classList.add("fa-eye")
-
+  if (currentType === "password") {
+    passwordField.setAttribute("type", "text");
+    showText.textContent = "HIDE";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
   }
 
-  if(currentType === "text") {
-    passwordField.setAttribute("type", "password")
-    showText.textContent = "SHOW"
-    eyeIcon.classList.remove("fa-eye")
-    eyeIcon.classList.add("fa-eye-slash")
-
+  if (currentType === "text") {
+    passwordField.setAttribute("type", "password");
+    showText.textContent = "SHOW";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
   }
-
-
-
 }
